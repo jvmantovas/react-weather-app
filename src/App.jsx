1c452fa6d3e55ee9de7dbb891a17fae5b7f21d64
jvmantovas <vmsjoao@gmail.com>
2022-12-12 21:53:27 -0300

    feat(topcities) TopButtons component with cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButtons from "./components/TopButtons";
 
 function App() {
   return (
-    <div className="mxauto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-gray-400"></div>
+    <div className="mxauto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-gray-400">
+      <TopButtons />
+    </div>
   );
 }
 
