52086ba1b51c7e5ce75efd8c2d348c7533d4f9fd
jvmantovas <vmsjoao@gmail.com>
2022-12-13 00:20:33 -0300

    feat(forecast) bottom forecast displays

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import Forecast from "./components/Forecast";
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-gray-400">
       <TopButtons />
       <Inputs />
+
       <TimeAndLocation />
       <TemperatureAndDetails />
+
+      <Forecast title="A cada hora" />
+      <Forecast title="Diário" />
     </div>
   );
 }
